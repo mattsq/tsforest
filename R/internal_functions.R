0cbe1d0d4d88e7deb399c4b2e377484fc06e3007
Matthew Simmons <matthewsimmons@192-168-1-101.tpgi.com.au>
2020-10-13 11:42:36 +1100

    Moving intervalwise_variable_importance plot function to exports, and a few fixes here and there.

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,8 +1,8 @@
-#' @importFrom stats lm
+#' @importFrom stats lm coef
 get_slope <- function(vec) {
   data <- data.frame(Y = vec, X = 1:length(vec))
   stats::lm(Y ~ X, data = data) %>%
-    coef() %>%
+    stats::coef() %>%
     {.[2]}
 }
 
@@ -32,4 +32,19 @@ featurize_df <- function(X_df, returned_object, verbose) {
   }
 
   featurized_df <- dplyr::bind_cols(all_features)
+}
+
+#' @importFrom tibble rownames_to_column
+#' @importFrom stringr str_extract str_remove_all str_remove
+scores_to_tibble <- function(scores) {
+  scores_df <- data.frame(
+    importance = scores
+  ) %>%
+    tibble::rownames_to_column() %>%
+    dplyr::mutate(From = stringr::str_extract(rowname, "From\\d+To"),
+                  To = stringr::str_extract(rowname, "To\\d+$"),
+                  From = stringr::str_remove_all(From, "From|To") %>% as.numeric(),
+                  To = stringr::str_remove_all(To, "To") %>% as.numeric(),
+                  Type = stringr::str_extract(rowname, "\\w+_\\d+") %>% stringr::str_remove("_\\d+"))
+  return(scores_df)
 }
